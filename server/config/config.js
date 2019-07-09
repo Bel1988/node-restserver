0c29ec62c42c6e908d07e4560346e309a98b3d53
Gustavo <belnem88@gmail.com>
2019-07-08 21:36:24 -0300

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -11,9 +11,22 @@ let urlDB;
 if (process.env.Node_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = process.env.MONGO_URI
+    urlDB = 'mongodb+srv://belphegor:Nemesis@4367@cluster0-h1n1s.mongodb.net/cafe'
 };
 
 process.env.URLDB = urlDB;
 
 //mongodb+srv://belphegor:Nemesis@4367@cluster0-h1n1s.mongodb.net/cafe
+
+
+
+// Vencimiento del Token
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 2 * 30;
+
+
+
+
+
+// SEED de autenticacion
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
